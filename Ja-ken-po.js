ce21c511f24d51c613bf5ac072caf8b82b869c9e
DiegoRT32 <sorpristerton43@proton.me>
2022-06-15 16:16:26 -0500

    Create game variables and Add randomizing function
    Create two variables.
    
    One variable contains the three possibilities, the other contains
    the result of the random function.
    
    Randomize array variable to then return it as the choice of the
    computer.

--- Ja-ken-po.js
+++ Ja-ken-po.js
@@ -1,1 +1,24 @@
-// 
+//The game is about picking rock, paper, scissors between the computer and a player
+
+let options = ["rock", "paper", "scissors"];
+const computerSelection = computerPlay();
+//The player must be able to choose between the three options (written or clickable) "playerSelection"
+
+
+//The computer must respond with a random string between those three options "computerPlay"
+function computerPlay(){
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+console.log (computerSelection);
+
+//Winner is defined depending on the result
+
+// rock beats scissors, scissors beats paper, paper beats rock. Else draw.
+//the result has to be shown 
+
+
+
+//the game must be 5 rounds 
+
+//print message for the player telling him the result
