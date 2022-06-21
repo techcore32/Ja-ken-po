5bbe74279ddb772124143d3e0d3e9168f1de54bd
DiegoRT32 <sorpristerton43@proton.me>
2022-06-20 20:56:34 -0500

    Create and style scoreboard for the game

--- Jan-ken-po.js
+++ Jan-ken-po.js
@@ -2,46 +2,46 @@
 
 const options = ["rock", "paper", "scissors"];
 
-const win = 0;
-const lost = 0;
+
 
 //The player must be able to choose between the three options (written or clickable) "playerSelection"
 //selection must compare and return expected result from the options variable
 
 
 const playerSelection =  document.querySelectorAll("[data-selection]");
+const message = document.querySelector(".result");
 
+const win = 0;
+const lost = 0;
 
 playerSelection.forEach(playerSelection => {
     playerSelection.addEventListener("click", e => {
         let selection = playerSelection.dataset.selection;
         makeSelection (selection);
-        
     }) 
-
-
 })
 
 function makeSelection(selection) {
 
     let computerSelection = computerPlay();
     let result = playRound(selection,computerSelection);
-
+    
+    message.innerText= result;
     console.log(selection);
     console.log(computerSelection);
     console.log(result);
 
-    if(result === "win"){
-        return win+1;
-    }
+    // if(result === "win"){
+    //     return win+1;
+    // }
 
-    else if (result === "lost") {
-        return lost+1;
-    }
+    // else if (result === "lost") {
+    //     return lost+1;
+    // }
 
-    else{
-        return ;
-    }
+    // else{
+    //     return ;
+    // }
     
     
         
@@ -55,25 +55,22 @@ function computerPlay(){
 
 
 
-//Winner is defined depending on the result
-
-// rock beats scissors, scissors beats paper, paper beats rock. Else draw.
-//the result has to be shown 
+//function defines the combinations that determine if you win, lose or draw
 
 
 function playRound(selection,computerSelection) {  
     if(selection === computerSelection){
-        return "draw";
+        return "It\'s a draw!";
     }
 
     else if (selection  === "scissors" && computerSelection === "paper" ||
-            selection  === "paper" && computerSelection === "rock" || 
-            selection  === "rock" && computerSelection === "scissors"){
-            return "win";  
-        }
-
-    else{
-        return "lost";
-    }
+    selection  === "paper" && computerSelection === "rock" || 
+    selection  === "rock" && computerSelection === "scissors"){
+    return " You win!";  
 }
- 
+
+else{
+return "Better luck next time!";
+}
+}
+
